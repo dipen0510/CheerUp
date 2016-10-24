f03adfd63d45fc0d0c7ffe5d2e72223e7ba34506
Dipen Sekhsaria <dipen0510@gmail.com>
2016-10-25 00:08:56 +0530

    Initial Commit

---
+++ CheerUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheerUp
+//
+//  Created by Dipen Sekhsaria on 25/10/16.
+//  Copyright © 2016 stardeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CheerUp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheerUp
+//
+//  Created by Dipen Sekhsaria on 25/10/16.
+//  Copyright © 2016 stardeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
